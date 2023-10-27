4900cbd539846f713c720b4efac0e92c18c5879d
samedfft2634 <samedfft@gmail.com>
2023-10-27 19:59:00 +0300

    api  changed

--- script.js
+++ script.js
@@ -1,12 +1,49 @@
 const loc = document.querySelector(".location");
-const searchBtn = document.querySelector(".searchBtn");
-const api = "SXER5KSpqQJlwqwWt6RRetDDIBCCDa7V";
-async function weather() {
-	const response = await fetch(
-		`https://api.tomorrow.io/v4/weather/forecast?location=${location.value}&apikey=${api}`
-	);
-	const data = await response.json();
-	console.log(data);
-}
-weather();
+			const searchBtn = document.querySelector(".searchBtn");
+			const apiKey = "feec9c8ff3dd920b17ff3d67c65a3910";
+			const apiUrl =
+				"https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+			searchBtn.addEventListener("click", async function () {
+				const city = loc.value.trim(); // trim() ile gereksiz boşlukları kaldırdık
+
+				if (!city) {
+					Swal.fire("Error!", "Please enter a city name!", "error");
+					return;
+				}
+
+				try {
+					const response = await fetch(
+						apiUrl + city + `&appid=${apiKey}`
+					);
+
+					if (response.ok) {
+						const data = await response.json();
+						console.log(data);
+					} else {
+						throw new Error(response.statusText);
+					}
+				} catch (err) {
+					Swal.fire("Hata!", err.message, "error");
+				}
+			});
 searchBtn.addEventListener("click", weather);
+// $("#kt_sweetalert_demo_3_1").click(function(e) {
+//     Swal.fire("Good job!", "You clicked the button!", "warning");
+// });
+
+// $("#kt_sweetalert_demo_3_2").click(function(e) {
+//     Swal.fire("Good job!", "You clicked the button!", "error");
+// });
+
+// $("#kt_sweetalert_demo_3_3").click(function(e) {
+//     Swal.fire("Good job!", "You clicked the button!", "success");
+// });
+
+// $("#kt_sweetalert_demo_3_4").click(function(e) {
+//     Swal.fire("Good job!", "You clicked the button!", "info");
+// });
+
+// $("#kt_sweetalert_demo_3_5").click(function(e) {
+//     Swal.fire("Good job!", "You clicked the button!", "question");
+// });
